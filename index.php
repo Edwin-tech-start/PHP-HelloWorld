1672b9940ea8bb9415d686235abb696c58da047d
Edwin-tech-start <67778977+Edwin-tech-start@users.noreply.github.com>
2021-01-09 23:30:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-1. This is my first page using OpenShift</p>'; ?> 
+ <?php echo '<p>Hello World-1. This is my first page using OpenShift [Edwin]</p>'; ?> 
  </body>
 </html>
